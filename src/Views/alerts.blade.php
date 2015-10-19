53775ceedbc18d3d5aac57e89d46970ae8d4449d
Benjamin Michotte <bmichotte@gmail.com>
2015-10-19 17:46:47 +0200

    case where callback is empty

--- src/Views/alerts.blade.php
+++ src/Views/alerts.blade.php
@@ -1,5 +1,9 @@
 @if (session()->has('flash_message'))
     <script>
+    	@if (empty(session('flash_message.callback')))
+    		swal({!! json_encode(session('flash_message.options')) !!});
+    	@else
         swal({!! json_encode(session('flash_message.options')) !!}, {!! session('flash_message.callback') !!});
+    	@endif	
     </script>
 @endif
